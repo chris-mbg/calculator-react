7008df8acd32626103f43383a6da1605e51d15d5
Chris <chrasse@hotmail.com>
2021-02-16 13:41:18 +0100

    Created calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import { calculator } from '../css/Calculator.module.css'
+
+const Calculator = () => {
+  return (
+    <div className={calculator}>
+      
+    </div>
+  );
+}
+
+export default Calculator;
