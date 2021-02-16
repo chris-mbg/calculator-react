928fd8630c68b553789e132ca66c5e580853ff4f
Chris <chrasse@hotmail.com>
2021-02-16 14:41:43 +0100

    Changes in ButtonsContainer
    Added values for Buttons and did some mapping

--- src/components/ButtonsContainer.js
+++ src/components/ButtonsContainer.js
@@ -1,9 +1,16 @@
 import { buttons } from '../css/ButtonsContainer.module.css'
+import { useState } from 'react';
 
 const ButtonsContainer = () => {
+  const [buttonValues, setButtonValues] = useState(
+    [7, 8, 9, '/', 4, 5, 6, 'X', 1, 2, 3, '-', 'clear', 0, '+', '=']
+  )
+
   return (
     <div className={buttons}>
-      <p>Here we will map Buttons later</p>
+      {buttonValues.map((value) => (
+        <p key={value}>{value}</p>
+      ))}
     </div>
   );
 }
