76463587b024b0ae3b00b58c84fd8648ec26a4a4
Chris <chrasse@hotmail.com>
2021-02-16 13:45:30 +0100

    Added calculator comp to Home

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,7 +3,7 @@ import { calculator } from '../css/Calculator.module.css'
 const Calculator = () => {
   return (
     <div className={calculator}>
-      
+      <h1>Calculator</h1>
     </div>
   );
 }
