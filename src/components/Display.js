5327dbbcae55aa48589ee2172b71470e8c112c87
Chris <chrasse@hotmail.com>
2021-02-16 15:54:53 +0100

    Made handleClick-func and made dataInput avaliable for Display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,13 @@
 import styles from '../css/Display.module.css'
+import { useContext} from 'react'
+import { DataContext } from '../contexts/DataContext'
 
 const Display = () => {
+  const { dataInput } = useContext(DataContext)
   return (
     <div className={styles.displayWrapper}>
       <div className={styles.display}>
-        <span className={styles.input}>Here goes the data</span>
+        <span className={styles.input}>{dataInput}</span>
       </div>
     </div>
   );
--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -7,12 +7,23 @@ const DataContextProvider = (props) => {
   const [dataInput, setDataInput] = useState('');
 
   const handleClick = (value) => {
-    console.log('Button pressed', value)
+    //console.log('Button pressed', value)
+    //Regex expression
+    if(/\d/.test(value)){
+      setDataInput(dataInput + value);    // Eftersom dataInput som default är en sträng görs value om till sträng
+    } else if ( value === 'clear'){
+      setDataInput('');
+    } else if (value === '=') {
+      console.log('Calculating...');
+    } else {
+      setDataInput(dataInput + ` ${value} `);
+    }
   }
 
 
   const values = {
-    handleClick,
+    dataInput,
+    handleClick
   }
 
   return (
