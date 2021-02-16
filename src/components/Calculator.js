13b698a01bd1a8c12749ac92d3e14312d7e92830
Chris <chrasse@hotmail.com>
2021-02-16 14:31:40 +0100

    Connected ButtonsContainer to Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,12 @@
 import { calculator } from '../css/Calculator.module.css'
+import ButtonsContainer from './ButtonsContainer';
 import Display from './Display';
 
 const Calculator = () => {
   return (
     <div className={calculator}>
       <Display />
-      <h1>ButtonsContainer component</h1>
+      <ButtonsContainer />
     </div>
   );
 }
