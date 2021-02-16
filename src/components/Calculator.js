082d6f36d691e1ffb907d7c9226644397e95a69a
Chris <chrasse@hotmail.com>
2021-02-16 14:52:49 +0100

    Created Button and ButtonsContainer
    Looped out Button in ButtonsContainer, hard coded values so far

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,12 @@
 import { calculator } from '../css/Calculator.module.css'
 import Display from './Display';
+import ButtonsContainer from './ButtonsContainer';
 
 const Calculator = () => {
   return (
     <div className={calculator}>
       <Display />
-      <h1>ButtonsContainer component</h1>
+      <ButtonsContainer />
     </div>
   );
 }
