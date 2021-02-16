99947df751fcd4849537c1d4e2aaf7fff126d2ef
Chris <chrasse@hotmail.com>
2021-02-16 14:26:40 +0100

    Created Buttons container comp

---
+++ src/components/ButtonsContainer.js
@@ -0,0 +1,11 @@
+import { buttons } from '../css/ButtonsContainer.module.css'
+
+const ButtonsContainer = () => {
+  return (
+    <div className={buttons}>
+      <p>Here we will map Buttons later</p>
+    </div>
+  );
+}
+
+export default ButtonsContainer;
