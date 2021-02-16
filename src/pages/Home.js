a8c4cdff7723ffa4d7d0859d2e8d8b098b3b2884
Chris <chrasse@hotmail.com>
2021-02-16 13:25:23 +0100

    Created a Home component

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import styles from '../css/Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <div className={styles.wrapper}>
+        <h1>Calculator be here!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
