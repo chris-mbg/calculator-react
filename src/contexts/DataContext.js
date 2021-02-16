fadfa7894a051c20f00b262bfc805451f199de64
Chris <chrasse@hotmail.com>
2021-02-16 16:05:18 +0100

    Added calculation-method. Calculator done.

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -15,11 +15,38 @@ const DataContextProvider = (props) => {
       setDataInput('');
     } else if (value === '=') {
       console.log('Calculating...');
+      calculate(dataInput)
     } else {
       setDataInput(dataInput + ` ${value} `);
     }
   }
 
+  const calculate = (calculation) => {
+    console.log(dataInput);
+    let calcArray = calculation.split(" ");
+    let operatorsArray = calcArray.filter((e) => /\D/.test(e)); // Båda dessa filter kommer inte hantera decimaltal.
+    let numbersArray = calcArray.filter((e) => !/\D/.test(e));
+    console.log(operatorsArray);
+    console.log(numbersArray);
+    let result = parseInt(numbersArray[0]);
+    for (let i = 0; i < numbersArray.length; i++) {
+      switch (operatorsArray[i]) {
+        case "+":
+          result += parseInt(numbersArray[i + 1]);
+          break;
+        case "-":
+          result -= numbersArray[i + 1];
+          break;
+        case "x":
+          result *= numbersArray[i + 1];
+          break;
+        case "/":
+          result /= numbersArray[i + 1];
+          break;
+      }
+    }
+    setDataInput(result);
+  };
 
   const values = {
     dataInput,
